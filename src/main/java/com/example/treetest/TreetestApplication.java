64391862475c8cc00cb0cb113a2688e23cbade06
DESKTOP-HAONQ08\JJPark <ehdqhddl91@gmail.com>
2021-10-24 20:31:09 +0900

    test

---
+++ src/main/java/com/example/treetest/TreetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.treetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreetestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreetestApplication.class, args);
+    }
+
+}
